59a96bd1a6d169d60e89e4d78ab285ae7975406b
yilins3 <sylfcx@gmail.com>
2018-09-12 18:09:28 -0500

    Solved the maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -20,7 +20,7 @@ public class SolveMaze {
         /*
          * Create a new 10 x 10 maze. Feel free to change these values.
          */
-        Maze maze = new Maze(10, 10);
+        Maze maze = new Maze(4, 4);
 
         /*
          * Pick (0, 0), the bottom left corner, as the starting point.
@@ -33,8 +33,32 @@ public class SolveMaze {
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
+        maze.canMove();
+        maze.move();
+        maze.turnRight();
+        maze.turnLeft();
+        maze.isFinished();
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnRight();
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnLeft();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnLeft();
+                        if (maze.canMove()) {
+                            maze.move();
+                        } else {
+                            maze.turnLeft();
+                            maze.move();
+                        }
+                    }
+                }
+            }
         }
 
         if (maze.isFinished()) {
